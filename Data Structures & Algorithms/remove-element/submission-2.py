269b9badf126ef750aaac4c6c33489f9f4c3e899
Reena Jennifer P <reenajprem@gmail.com>
2026-05-12 21:18:53 +0530

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, arr: List[int], val: int) -> int:
+        i=0
+        for j in range(len(arr)):
+            if arr[j] != val:
+                arr[i] = arr[j]
+                i+=1
+        return i
